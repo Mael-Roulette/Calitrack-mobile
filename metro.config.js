9aacad686a1473298a253e3b0f821fc23d473e6a
cBasique <maelroulette12@gmail.com>
2025-09-10 09:23:25 +0200

    back to dev

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-config.watchFolders = [path.resolve(__dirname, 'node_modules')];
-
 module.exports = withNativeWind(config, { input: "./app/globals.css" });
