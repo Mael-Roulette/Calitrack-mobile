3f1e1f2b000b6eefdc6f0e30ab1160cd9025d27b
cBasique <maelroulette12@gmail.com>
2025-09-22 18:08:14 +0200

    update web

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -21,31 +21,33 @@ const AuthLayout = () => {
 		<KeyboardAvoidingView
 			behavior={Platform.OS === "ios" ? "padding" : "height"}
 		>
-			<ScrollView
-				className='bg-background h-full'
-				keyboardShouldPersistTaps='handled'
-			>
-				<View
-					className='bg-primary overflow-hidden'
-					style={{ height: Dimensions.get("screen").height / 3 }}
+			<View className="bg-background flex-1">
+				<ScrollView
+					className='bg-background h-full'
+					keyboardShouldPersistTaps='handled'
 				>
-					<View className='justify-center items-center flex-1 mt-16'>
-						<Image
-							source={require("../../assets/images/logo.png")}
-							style={{ width: 180, height: 100 }}
-							resizeMode='contain'
-						/>
-					</View>
+					<View
+						className='bg-primary overflow-hidden'
+						style={{ height: Dimensions.get("screen").height / 3 }}
+					>
+						<View className='justify-center items-center flex-1 mt-16'>
+							<Image
+								source={require("../../assets/images/logo.png")}
+								style={{ width: 180, height: 100 }}
+								resizeMode='contain'
+							/>
+						</View>
 
-					<Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
-						<Path
-							d={`M0,${height} Q${width / 2},0 ${width},${height} L${width},${height * 2} L0,${height * 2} Z`}
-							fill='#FFF9F7'
-						/>
-					</Svg>
-				</View>
-				<Slot />
-			</ScrollView>
+						<Svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
+							<Path
+								d={`M0,${height} Q${width / 2},0 ${width},${height} L${width},${height * 2} L0,${height * 2} Z`}
+								fill='#FFF9F7'
+							/>
+						</Svg>
+					</View>
+					<Slot />
+				</ScrollView>
+			</View>
 		</KeyboardAvoidingView>
 	);
 };
