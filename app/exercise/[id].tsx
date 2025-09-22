3d8fc8d462b35824550b5940b4ab925b710fe132
cBasique <maelroulette12@gmail.com>
2025-09-22 16:48:54 +0200

    adding safe area view for exercise

--- app/exercise/[id].tsx
+++ app/exercise/[id].tsx
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { getExerciseImage } from '@/constants/exercises';
 import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ExerciseDetails = () => {
 	const { id } = useLocalSearchParams();
@@ -42,52 +43,54 @@ const ExerciseDetails = () => {
 	}, [ navigation ] );
 
 	return (
-		<View className='flex-1 bg-background'>
-			{ loading ? (
-				<View className='flex-1 items-center justify-center'>
-					<ActivityIndicator size='large' color='#0000ff' />
-					<Text>Chargement...</Text>
-				</View>
-			) : (
-				<ScrollView>
-					<View className='relative bg-secondary px-4 py-8 rounded-b-2xl min-h-80 w-full items-center justify-center'>
-						{ exercise.image ? (
-							<Image
-								source={ getExerciseImage( exercise.image ) }
-								style={ { width: 250, height: 250 } }
-								contentFit="contain"
-							/>
-						) : (
-							<Text className="title-4 text-background text-center">Illustration en cours de création…</Text>
-						) }
-
-						<View className='absolute top-4 left-4 z-10'>
-							<TouchableOpacity
-								onPress={ () => router.back() }
-								className='w-10 h-10 rounded-full items-center justify-center'
-							>
-								<Ionicons name="arrow-back" size={ 24 } color="#FFF9F7" />
-							</TouchableOpacity>
-						</View>
+		<SafeAreaView className='flex-1 bg-background'>
+			<View className='flex-1'>
+				{ loading ? (
+					<View className='flex-1 items-center justify-center'>
+						<ActivityIndicator size='large' color='#0000ff' />
+						<Text>Chargement...</Text>
 					</View>
+				) : (
+					<ScrollView>
+						<View className='relative bg-secondary px-4 py-8 rounded-b-2xl min-h-80 w-full items-center justify-center'>
+							{ exercise.image ? (
+								<Image
+									source={ getExerciseImage( exercise.image ) }
+									style={ { width: 250, height: 250 } }
+									contentFit="contain"
+								/>
+							) : (
+								<Text className="title-4 text-background text-center">Illustration en cours de création…</Text>
+							) }
 
-					<View className='px-5 mt-6 mb-4'>
-						<Text className="title-2 mb-4">{ exercise.name }</Text>
-						<View className='flex-row items-center justify-between mb-3'>
-							<Text className='text'>
-								Type :{ " " }
-								<Text className='text-secondary'>{ exercise.type.name }</Text>
-							</Text>
-							<Text className='text'>
-								Difficulté :{ " " }
-								<Text className='text-secondary'>{ exercise.difficulty }</Text>
-							</Text>
+							<View className='absolute top-4 left-4 z-10'>
+								<TouchableOpacity
+									onPress={ () => router.back() }
+									className='w-10 h-10 rounded-full items-center justify-center'
+								>
+									<Ionicons name="arrow-back" size={ 24 } color="#FFF9F7" />
+								</TouchableOpacity>
+							</View>
 						</View>
-						<Text className='text mt-2'>{ exercise.description }</Text>
-					</View>
-				</ScrollView>
-			) }
-		</View>
+
+						<View className='px-5 mt-6 mb-4'>
+							<Text className="title-2 mb-4">{ exercise.name }</Text>
+							<View className='flex-row items-center justify-between mb-3'>
+								<Text className='text'>
+									Type :{ " " }
+									<Text className='text-secondary'>{ exercise.type.name }</Text>
+								</Text>
+								<Text className='text'>
+									Difficulté :{ " " }
+									<Text className='text-secondary'>{ exercise.difficulty }</Text>
+								</Text>
+							</View>
+							<Text className='text mt-2'>{ exercise.description }</Text>
+						</View>
+					</ScrollView>
+				) }
+			</View>
+		</SafeAreaView>
 	);
 };
 
