cf63daf795de2d2d576383b3b8861fb073e3ad36
Mael Roulette <maelroulette12@gmail.com>
2026-01-30 21:48:59 +0100

    added eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,41 @@
-// https://docs.expo.dev/guides/using-eslint/
+// eslint.config.js
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
 
-module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = defineConfig([
+    ...expoConfig,
+    {
+        files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
+        ignores: [
+            "node_modules/**",
+            "dist/**",
+            ".expo/**",
+            "android/**",
+            "ios/**",
+            "*.config.js",
+            "*.config.ts"
+        ],
+        rules: {
+            "semi": ["error", "always"],
+            "space-before-blocks": ["error", "always"],
+            "space-before-function-paren": ["error", "always"],
+            "array-bracket-spacing": ["error", "always"],
+            "object-curly-spacing": ["error", "always"],
+            "react/jsx-curly-spacing": ["error", { "when": "always" }],
+            "space-in-parens": ["error", "always"],
+            "indent": ["error", 4, { "SwitchCase": 1 }],
+            "react/jsx-indent": ["error", 4],
+            "quotes": ["error", "double"],
+            "space-infix-ops": "error",
+            "no-multiple-empty-lines": ["error", { "max": 2 }],
+            "eqeqeq": ["error", "always"],
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": ["warn"],
+            "react/jsx-tag-spacing": ["error", { "beforeSelfClosing": "always" }],
+            "computed-property-spacing": ["error", "always"],
+        },
+    },
 ]);
+
+module.exports = eslintConfig;
