03bca5a45338e9e7565a5398e5e612a6c0edf5b7
Mael <maelroulette12@gmail.com>
2026-02-15 11:25:34 +0100

    Fix page header

--- components/headers/PageHeader.tsx
+++ components/headers/PageHeader.tsx
@@ -32,7 +32,7 @@ export default function PageHeader ( {
           <Ionicons name="arrow-back" size={ 28 } color="white" />
         </TouchableOpacity>
 
-        <Text className="title text-background">{title}</Text>
+        <Text className="title text-background flex-1 truncate" numberOfLines={ 1 }>{title}</Text>
       </View>
     </HeaderContainer>
   );
