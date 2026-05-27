1db02878c9955e3a781b90d13c7285568d97a601
Mael <maelroulette12@gmail.com>
2026-05-27 23:13:17 +0200

    variant app configuration

---
+++ app.config.js
@@ -0,0 +1,74 @@
+const variant = process.env.APP_VARIANT;
+const IS_DEV = variant === 'development';
+const IS_PREVIEW = variant === 'preview';
+
+const appName = IS_DEV
+  ? 'Calitrack (Dev)'
+  : IS_PREVIEW
+    ? 'Calitrack (Preview)'
+    : 'Calitrack';
+
+export default {
+  name: appName,
+  slug: 'calitrack-ts',
+  version: '0.9.6',
+  orientation: 'portrait',
+  icon: './assets/images/logo-fond.png',
+  scheme: 'calitrackts',
+  userInterfaceStyle: 'automatic',
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.mael_rltt.calitrackts',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/images/adaptive-icon.png',
+      backgroundColor: '#FFF9F7',
+    },
+    edgeToEdgeEnabled: true,
+    package: 'com.mael_rltt.calitrackts',
+    googleServicesFile: IS_DEV ? './google-services-dev.json' : './google-services.json',
+  },
+  web: {
+    bundler: 'metro',
+    output: 'static',
+    favicon: './assets/images/adaptive-icon.png',
+  },
+  plugins: [
+    'expo-router',
+    [
+      'expo-splash-screen',
+      {
+        image: './assets/images/adaptive-icon.png',
+        imageWidth: 200,
+        resizeMode: 'contain',
+        backgroundColor: '#FFF9F7',
+        dark: {
+          backgroundColor: '#FFF9F7',
+        },
+      },
+    ],
+    [
+      'expo-image-picker',
+      {
+        photosPermission:
+          "L'application nécessite l'accès à vos photos pour que vous puissiez choisir une image de profil.",
+      },
+    ],
+    'expo-notifications',
+    '@react-native-firebase/app',
+    'expo-asset',
+    'expo-web-browser',
+  ],
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    EXPO_ROUTER_APP_ROOT: './app',
+    router: {},
+    eas: {
+      projectId: 'aa0ea6c8-f82d-4087-a86b-4e88c39288c4',
+    },
+  },
+};
